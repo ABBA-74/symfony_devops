376d3bad81e4c8018b59229d0856974216718012
Abdel Bailoul <ab.bailoul@gmail.com>
2024-11-27 02:54:53 +0100

    feat: add buttons to handle sliders on github workflow process section

--- assets/js/anim-sliders.js
+++ assets/js/anim-sliders.js
@@ -1,10 +1,48 @@
-const sliders = document.querySelectorAll('.section-3 .wrapper-illustration-xl .wrapper-img')
+const sliders = document.querySelectorAll(
+  '.section-3 .wrapper-illustration-xl .wrapper-img'
+);
+const subtitles = document.querySelectorAll(
+  '.section-3 .wrapper-illustration-xl .illustration-subtitle'
+)
+const nextSlideBtn = document.querySelector('#nextSlideBtn');
+const prevSlideBtn = document.querySelector('#prevSlideBtn');
+let idInterval;
+let i = 0;
 
-let i=0;
-setInterval(() => {
-    sliders.forEach(el => el.classList.remove('active'))
-    sliders[i].classList.add('active')
-    i++;
-    i = i >= sliders.length ? 0 : i;
-    console.log(i)
-}, 5200);
+const updateSlide = (i) => {
+  sliders.forEach((el) => el.classList.remove('active'));
+  subtitles.forEach((el) => el.classList.remove('active'));
+  sliders[i].classList.add('active');
+  subtitles[i].classList.add('active');
+};
+const nextSlide = () => {
+  i++;
+  i = i >= sliders.length ? 0 : i;
+  updateSlide(i);
+};
+
+const prevSlide = () => {
+  i--;
+  i = i < 0 ? sliders.length - 1 : i;
+  updateSlide(i);
+};
+
+const startAutoSlide = () => {
+  idInterval = setInterval(() => {
+    nextSlide();
+  }, 5200);
+};
+
+nextSlideBtn.addEventListener('click', () => {
+  clearInterval(idInterval);
+  nextSlide();
+  startAutoSlide();
+});
+
+prevSlideBtn.addEventListener('click', () => {
+  clearInterval(idInterval);
+  prevSlide();
+  startAutoSlide();
+});
+
+startAutoSlide();
